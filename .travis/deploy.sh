b95323712c639fc7b65873a38da516f487e6d57b
Kousuke Sugano <sinner.shiki.g@gmail.com>
2016-02-22 01:14:46 +0900

    change deploy.sh and travis.yml

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,5 +3,4 @@ eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy_key
 ssh-add .travis/deploy_key
 git remote add deploy ssh://sugano@sinnershiki.com:10022/home/sugano/portfolio.git
-git reset --hard
 git push deploy master
