2a32cc8b477090f1ce0b583af1e212d6844171d6
Kousuke Sugano <sinner.shiki.g@gmail.com>
2016-02-21 23:53:01 +0900

    test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,4 +3,5 @@ eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy_key
 ssh-add .travis/deploy_key
 git remote add deploy ssh://sugano@sinnershiki.com:10022/home/sugano/portfolio.git
+git reset --hard
 git push deploy master
